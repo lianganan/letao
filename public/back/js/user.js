b446071f791ac91c214c5e5ca33da33db3d5743d
lianganan <894795786@qq.com>
2018-09-28 14:38:50 +0800

    用户管理分页功能完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,15 +1,26 @@
-$(function () {
-
-  $.ajax({
-    type: "get",
-    url: "/user/queryUser",
-    data:{page:1,pageSize:5},
-    dataType: "json",
-    success: function (response) {
-      console.log(response);
-      $('tbody').html(template('tmp',response))
-    }
-  });
-
-
-})
+$(function() {
+  function ready(page,pageSize) {
+    $.ajax({
+      type: "get",
+      url: "/user/queryUser",
+      data: { page: page || 1, pageSize: pageSize || 5 },
+      dataType: "json",
+      success: function(response) {
+        console.log(response);
+        totalPages = Math.ceil(response.total / response.size);
+        // console.log(totalPages);
+        // 分页
+        $("#pagination").bootstrapPaginator({
+          bootstrapMajorVersion: 3,
+          totalPages: Math.ceil(response.total / response.size),
+          currentPage: response.page,
+          onPageClicked: function(e, o, t, page) {
+            ready(page);
+          }
+        });
+        $("tbody").html(template("tmp", response));
+      }
+    });
+  }
+  ready();
+});
